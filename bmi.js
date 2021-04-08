e41f4fe496e10d3dd491c30dcf9e01f45791130a
shivanandi007 <shivanandi880@gmail.com>
2021-04-08 13:04:36 +0530

    hi shiva bmi here

---
+++ bmi.js
@@ -0,0 +1,23 @@
+function bmical()
+{
+	var height = document.bmireport.height.value
+	var weight =document.bmireport.weight.value
+	if(weight > 0 && height > 0){	
+    var finalBmi = weight/(height*height)
+    document.bmireport.bmi.value = finalBmi
+    if(finalBmi < 18.5){
+    document.bmireport.meaning.value = "That you are too thin."
+    }
+    if(finalBmi > 18.5 && finalBmi < 25){
+    document.bmireport.meaning.value = "That you are healthy."
+    }
+    if(finalBmi > 25){
+    document.bmireport.meaning.value = "That you have overweight."
+    }
+    }
+    else{
+    alert("Please Fill in everything correctly")
+    }
+    }
+
+	
